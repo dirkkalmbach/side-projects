292c7ab4788a1edd6f29c77de5c68c43131f825f
dirkkalmbach <dirk.kalmbach@gmail.com>
2023-03-10 21:09:00 +0800

    feat: Add filename renaming from mp4 to mp3

--- 😎 private tasks/download-youtube-and-upload-to-podcast-player/download_from_youtube_and_upload_to_overcast.py
+++ 😎 private tasks/download-youtube-and-upload-to-podcast-player/download_from_youtube_and_upload_to_overcast.py
@@ -1,13 +1,23 @@
 #%%
 # Import Libraries
 from pytube import YouTube
-
+import os
 
 # 👇🏻 C H A N G E 👇🏻
 SAVE_PATH_FOLDER = "/Users/dirkkalmbach/Downloads" # <- folder
 # 👆🏻👆🏻👆🏻👆🏻👆🏻👆🏻👆🏻👆🏻
 
+# download
+print("downloading from youtube ...")
+video_url = "https://www.youtube.com/watch?v=_UxECPYSWVs"
 yt = YouTube(video_url)
 t=yt.streams.filter(only_audio=True)
+path = t[0].download(SAVE_PATH_FOLDER)
 
-t[0].download(SAVE_PATH_FOLDER)
+# change file extension mp4 -> mp3
+try:
+    os.rename(path, path[:-3]+".mp3")
+except:
+    print("Error: could not change file ending to .mp3")
+
+# %%
