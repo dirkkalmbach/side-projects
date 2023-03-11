6c9143acbcf7278c9746cb973e4a57e68b8174f0
dirkkalmbach <dirk.kalmbach@gmail.com>
2023-03-11 08:52:38 +0800

    feat: add arguments functionality for parsing youtube url

--- 😎 private tasks/download-youtube-and-upload-to-podcast-player/download_from_youtube_and_upload_to_overcast.py
+++ 😎 private tasks/download-youtube-and-upload-to-podcast-player/download_from_youtube_and_upload_to_overcast.py
@@ -2,14 +2,22 @@
 # Import Libraries
 from pytube import YouTube
 import os
+import argparse
+
 
 # 👇🏻 C H A N G E 👇🏻
 SAVE_PATH_FOLDER = "/Users/dirkkalmbach/Downloads" # <- folder
 # 👆🏻👆🏻👆🏻👆🏻👆🏻👆🏻👆🏻👆🏻
 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--name", required=True,
+	help="url of youtube video")
+args = vars(ap.parse_args())
+
 # download
+video_url = args["name"]#"https://www.youtube.com/watch?v=_UxECPYSWVs"
 print("downloading from youtube ...")
-video_url = "https://www.youtube.com/watch?v=_UxECPYSWVs"
 yt = YouTube(video_url)
 t=yt.streams.filter(only_audio=True)
 path = t[0].download(SAVE_PATH_FOLDER)
